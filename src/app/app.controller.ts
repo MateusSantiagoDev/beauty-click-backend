df76e30977385ef9d7f07a0e8e88ee1ccae98c9b
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-23 18:55:50 -0300

    feat: update app module

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger/dist';
 import { AppService } from './app.service';
 
-@Controller()
+@ApiTags('Status')
+@Controller('status')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { SignupModule } from '../signup/signup.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, SignupModule],
   controllers: [AppController],
   providers: [AppService],
 })
