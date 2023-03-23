3a5c6488ff53f05896b35ea19747aa2f0313bbac
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-23 18:55:10 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle('BeautyClick')
+  .setDescription('Sistema Completo de Gestão de Salão de Beleza')
+  .setVersion('1.0.0')
+  .addTag('Status')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
