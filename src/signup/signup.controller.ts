d4d0def44fd4884bfbabd0ea6896391c5ee5d3fd
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-24 12:43:10 -0300

    feat: update signup controller

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -1,7 +1,73 @@
-import { Controller } from "@nestjs/common";
-import { SignupService } from "./signup.service";
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user-entity';
+import { SignupService } from './signup.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
-@Controller()
+@ApiTags('Signup')
+@Controller('signup')
 export class SignupController {
-    constructor (private readonly service: SignupService) {}
+  constructor(private readonly service: SignupService) {}
+
+  @ApiOperation({
+    summary: 'Cadastrar um usuário',
+  })
+  @Post()
+  async create(@Body() dto: CreateUserDto): Promise<UserEntity> {
+    try {
+      return await this.service.create(dto);
+    } catch (error) {}
+  }
+
+  @ApiOperation({
+    summary: 'Buscar todos os usuário',
+  })
+  @Get()
+  async findAll(): Promise<UserEntity[]> {
+    try {
+      return await this.service.findAll();
+    } catch (error) {}
+  }
+
+  @ApiOperation({
+    summary: 'Buscar um usuário pelo ID',
+  })
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<UserEntity> {
+    try {
+      return await this.service.findOne(id);
+    } catch (error) {}
+  }
+
+  @ApiOperation({
+    summary: 'Atualizar um usuário pelo ID',
+  })
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateUserDto,
+  ): Promise<UserEntity> {
+    try {
+      return await this.service.update(id, dto);
+    } catch (error) {}
+  }
+
+  @ApiOperation({
+    summary: 'Remover um usuário pelo ID',
+  })
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<UserEntity> {
+    try {
+      return await this.service.delete(id);
+    } catch (error) {}
+  }
 }
--- src/signup/signup.service.ts
+++ src/signup/signup.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 import { SignupRepository } from "./repository/signup.repository";
 
 @Injectable()
 export class SignupService {
+    delete(id: string): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
+        throw new Error('Method not implemented.');
+    }
+    update(id: string, dto: UpdateUserDto): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
+        throw new Error('Method not implemented.');
+    }
+    findOne(id: string): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
+        throw new Error('Method not implemented.');
+    }
+    findAll(): import("./entities/user-entity").UserEntity[] | PromiseLike<import("./entities/user-entity").UserEntity[]> {
+        throw new Error('Method not implemented.');
+    }
+    create(dto: CreateUserDto): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
+        throw new Error('Method not implemented.');
+    }
     constructor (private readonly repository: SignupRepository) {}
 }
