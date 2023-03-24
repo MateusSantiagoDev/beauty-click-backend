971979289dc26a6a25e6225bd8f41d366eb2ed13
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-24 12:49:30 -0300

    feat: update signup service

--- src/signup/repository/signup.repository.ts
+++ src/signup/repository/signup.repository.ts
@@ -1,7 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../../prisma/prisma.service";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
 
 Injectable()
 export class SignupRepository {
+    delete(id: string): import("../entities/user-entity").UserEntity | PromiseLike<import("../entities/user-entity").UserEntity> {
+        throw new Error("Method not implemented.");
+    }
+    update(id: string, dto: UpdateUserDto): import("../entities/user-entity").UserEntity | PromiseLike<import("../entities/user-entity").UserEntity> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: string): import("../entities/user-entity").UserEntity | PromiseLike<import("../entities/user-entity").UserEntity> {
+        throw new Error("Method not implemented.");
+    }
+    finaAll(): import("../entities/user-entity").UserEntity[] | PromiseLike<import("../entities/user-entity").UserEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+    create(dto: CreateUserDto): Promise<UserEntity> {
+        throw new Error("Method not implemented.");
+    }
     constructor (private readonly prisma: PrismaService) {}
 }
--- src/signup/signup.service.ts
+++ src/signup/signup.service.ts
@@ -1,24 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
-import { SignupRepository } from "./repository/signup.repository";
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user-entity';
+import { SignupRepository } from './repository/signup.repository';
 
 @Injectable()
 export class SignupService {
-    delete(id: string): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
-        throw new Error('Method not implemented.');
-    }
-    update(id: string, dto: UpdateUserDto): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
-        throw new Error('Method not implemented.');
-    }
-    findOne(id: string): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
-        throw new Error('Method not implemented.');
-    }
-    findAll(): import("./entities/user-entity").UserEntity[] | PromiseLike<import("./entities/user-entity").UserEntity[]> {
-        throw new Error('Method not implemented.');
-    }
-    create(dto: CreateUserDto): import("./entities/user-entity").UserEntity | PromiseLike<import("./entities/user-entity").UserEntity> {
-        throw new Error('Method not implemented.');
-    }
-    constructor (private readonly repository: SignupRepository) {}
+  constructor(private readonly repository: SignupRepository) {}
+  async create(dto: CreateUserDto): Promise<UserEntity> {
+    return await this.repository.create(dto);
+  }
+  async findAll(): Promise<UserEntity[]> {
+    return await this.repository.finaAll();
+  }
+  async findOne(id: string): Promise<UserEntity> {
+    return await this.repository.findOne(id);
+  }
+  async update(id: string, dto: UpdateUserDto): Promise<UserEntity> {
+    return await this.repository.update(id, dto);
+  }
+  async delete(id: string): Promise<UserEntity> {
+    return await this.repository.delete(id);
+  }
 }
