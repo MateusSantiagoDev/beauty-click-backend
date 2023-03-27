045aaa6defc0dc719086ab0f304e62bb930c81df
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-27 10:45:11 -0300

    feat: update signup service

--- src/signup/signup.service.ts
+++ src/signup/signup.service.ts
@@ -1,25 +1,109 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserEntity } from './entities/user-entity';
-import { SignupRepository } from './repository/signup.repository';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class SignupService {
-  constructor(private readonly repository: SignupRepository) {}
+  constructor(private readonly prisma: PrismaService) {}
+
   async create(dto: CreateUserDto): Promise<UserEntity> {
-    return await this.repository.create(dto);
+    if (dto.password !== dto.confirmPassword) {
+      throw new Error('Senha invalida!');
+    }
+    delete dto.confirmPassword;
+    
+    const userId = randomUUID();
+    const addressId = randomUUID();
+
+    const result = await this.prisma.$transaction(async (prisma) => {
+      const userData = await prisma.user.create({
+        data: {
+          id: userId,
+          name: dto.name,
+          email: dto.email,
+          cpf: dto.cpf,
+          password: dto.password,
+          role: dto.role,
+          createdAt: new Date(),
+        },
+      });
+      const addressData = await prisma.address.create({
+        data: {
+          id: addressId,
+          cep: dto.address.cep,
+          district: dto.address.district,
+          road: dto.address.road,
+          number: dto.address.number,
+          user: { connect: { id: userData.id } },
+        },
+      });
+      return { ...userData, address: addressData };
+    });
+
+    delete result.updatedAt;
+
+    return result;
   }
+
   async findAll(): Promise<UserEntity[]> {
-    return await this.repository.finaAll();
+    return await this.prisma.user.findMany({
+      include: {
+        address: true,
+      }
+    })
   }
+
   async findOne(id: string): Promise<UserEntity> {
-    return await this.repository.findOne(id);
+    return await this.prisma.user.findFirstOrThrow({ where: { id }, include: {
+      address: true,
+    }})
   }
-  async update(id: string, dto: UpdateUserDto): Promise<UserEntity> {
-    return await this.repository.update(id, dto);
+async update(id: string, dto: UpdateUserDto): Promise<UserEntity> {
+    await this.findOne(id);
+    if (dto.password) {
+      if (dto.password !== dto.confirmPassword) {
+        throw new Error('Senha invalida!');
+      }
+    }
+    delete dto.confirmPassword;
+
+    const result = await this.prisma.$transaction(async (prisma) => {
+      const userUpdateData = {
+        name: dto.name,
+        email: dto.email,
+        cpf: dto.cpf,
+        password: dto.password,
+        role: dto.role,
+        updatedAt: new Date(),
+      };
+      const addressUpdateData = {
+        cep: dto.address.cep,
+        district: dto.address.district,
+        road: dto.address.road,
+        number: dto.address.number,
+      };
+      const updatedUser = await prisma.user.update({
+        where: { id },
+        data: userUpdateData,
+      });
+      const updatedAddress = await prisma.address.update({
+        where: { userId: id },
+        data: addressUpdateData,
+      });
+      return { ...updatedUser, address: updatedAddress };
+    });
+    return result;
   }
-  async delete(id: string): Promise<UserEntity> {
-    return await this.repository.delete(id);
+
+  async delete(id: string) {
+    await this.prisma.$transaction(async (prisma) => {
+      await prisma.address.delete({ where: { 
+        userId: id,
+      }})
+      await prisma.user.delete({ where: { id }})
+    })
   }
 }
