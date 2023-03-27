dd4983f76ef7ea297951fe8ac84285e30ab4ba33
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-27 10:42:08 -0300

    feat: update dto

--- src/signup/dto/create-user.dto.ts
+++ src/signup/dto/create-user.dto.ts
@@ -1,40 +1,74 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Address } from '../entities/user-entity';
+
+export class CreateAddressDto {
+  @ApiProperty({
+    description: 'Cep',
+    example: '0000000'
+  })
+  cep: string;
+
+  @ApiProperty({
+    description: 'Bairro',
+    example: 'Jardim exemplo'
+  })
+  district: string;
+
+  @ApiProperty({
+    description: 'Rua',
+    example: 'Rua exemplo'
+  })
+  road: string;
+
+  @ApiProperty({
+    description: 'número da residência',
+    example: 300
+  })
+  number: number;
+
+  @ApiProperty({
+    description: 'ID do usuário ao qual o endereço pertence',
+    example: '1234'
+  })
+  userId: string;
+}
 
 export class CreateUserDto {
   @ApiProperty({
-    description: 'nome de usuário',
+    description: 'Nome do usuário',
+    example: 'Mateus'
   })
   name: string;
 
   @ApiProperty({
-    description: 'Email do usuário',
+    description: 'Email',
+    example: 'exemplo@exemplo.com'
   })
   email: string;
 
   @ApiProperty({
-    description: 'CPF do usuário',
+    description: 'CPF',
+    example: '00000000000'
   })
   cpf: string;
 
   @ApiProperty({
-    description: 'Senha do usuário',
+    description: 'Senha',
+    example: '1234@'
   })
   password: string;
 
   @ApiProperty({
     description: 'Confirmação da senha',
+    example: '1234@'
   })
   confirmPassword: string;
 
   @ApiProperty({
-    description: 'permissões de usuário',
+    description: 'Permissão do usuário',
+    example: 'user'
   })
   role: string;
 
-  @ApiProperty({
-    description: 'Endereço do usuário',
-    type: ['Address'],
-  })
-  address: Address[];
+  @ApiProperty({ type: CreateAddressDto })
+  address: CreateAddressDto;
 }
