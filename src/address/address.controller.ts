2b783530f0fa055232e67e7d06a7221af72be604
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-28 18:37:05 -0300

    feat: add methods to AddressController

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,7 +1,80 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { HandleExceptions } from 'src/utils/exceptions/handle-exceptions';
 import { AddressService } from './address.service';
+import { CreateAddressDto } from './dto/create-address.dto';
+import { UpdateAddressDto } from './dto/update-address.dto';
+import { AddressEntity } from './entities/address-entity';
 
-@Controller()
+@ApiTags('Address')
+@Controller('address')
 export class AddressController {
-    constructor(private readonly service: AddressService) {}
+  constructor(private readonly service: AddressService) {}
+
+  @ApiOperation({
+    summary: 'Adicionar um novo endereço',
+  })
+  @Post()
+  async create(@Body() dto: CreateAddressDto): Promise<AddressEntity> {
+    try {
+      return await this.service.create(dto);
+    } catch (error) {
+      HandleExceptions(error);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Buscar todos os endereços',
+  })
+  @Get()
+  async findAll(): Promise<AddressEntity[]> {
+    try {
+      return await this.service.findAll();
+    } catch (error) {
+      HandleExceptions(error);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Buscar um endereço por ID',
+  })
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<AddressEntity> {
+    try {
+      return await this.service.findOne(id);
+    } catch (error) {
+      HandleExceptions(error);
+    }
+  }
+  @ApiOperation({
+    summary: 'Atualizar um endereço por ID',
+  })
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateAddressDto,
+  ): Promise<AddressEntity> {
+    try {
+      return await this.service.update(id, dto);
+    } catch (error) {
+      HandleExceptions(error);
+    }
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<AddressEntity> {
+    try {
+      return await this.service.delete(id);
+    } catch (error) {
+      HandleExceptions(error);
+    }
+  }
 }
