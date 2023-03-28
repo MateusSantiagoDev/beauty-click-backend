d5dd6f92f644bc4530f04185c881a03edfb204bc
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-28 17:48:24 -0300

    feat: create address dto

---
+++ src/address/dto/create-address.dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateAddressDto {
+  @ApiProperty({
+    description: 'Nome do salão',
+    example: 'Salão nota 10',
+  })
+  name: string;
+
+  @ApiProperty({
+    description: 'Imagem do salão',
+    example: 'http://foto.com',
+  })
+  image: string;
+
+  @ApiProperty({
+    description: 'Cep',
+    example: '0000000',
+  })
+  cep: string;
+
+  @ApiProperty({
+    description: 'Bairro',
+    example: 'Jardim exemplo',
+  })
+  district: string;
+
+  @ApiProperty({
+    description: 'Rua',
+    example: 'Rua exemplo',
+  })
+  road: string;
+
+  @ApiProperty({
+    description: 'número da residência',
+    example: 300,
+  })
+  number: number;
+}
