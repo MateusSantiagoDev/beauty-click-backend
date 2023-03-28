89ce7c02c1f8ee3eba8259ef8db370716fd2dbf3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-28 15:03:52 -0300

    refactor: update dto

--- src/signup/dto/create-user.dto.ts
+++ src/signup/dto/create-user.dto.ts
@@ -1,74 +1,62 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateAddressDto {
-  @ApiProperty({
-    description: 'Cep',
-    example: '0000000'
-  })
-  cep: string;
-
-  @ApiProperty({
-    description: 'Bairro',
-    example: 'Jardim exemplo'
-  })
-  district: string;
-
-  @ApiProperty({
-    description: 'Rua',
-    example: 'Rua exemplo'
-  })
-  road: string;
-
-  @ApiProperty({
-    description: 'número da residência',
-    example: 300
-  })
-  number: number;
-
-  @ApiProperty({
-    description: 'ID do usuário ao qual o endereço pertence',
-    example: '1234'
-  })
-  userId: string;
-}
-
 export class CreateUserDto {
   @ApiProperty({
     description: 'Nome do usuário',
-    example: 'Mateus'
+    example: 'Mateus',
   })
   name: string;
 
   @ApiProperty({
     description: 'Email',
-    example: 'exemplo@exemplo.com'
+    example: 'exemplo@exemplo.com',
   })
   email: string;
 
   @ApiProperty({
     description: 'CPF',
-    example: '00000000000'
+    example: '00000000000',
   })
   cpf: string;
 
   @ApiProperty({
     description: 'Senha',
-    example: '1234@'
+    example: '1234@',
   })
   password: string;
 
   @ApiProperty({
     description: 'Confirmação da senha',
-    example: '1234@'
+    example: '1234@',
   })
   confirmPassword: string;
 
   @ApiProperty({
     description: 'Permissão do usuário',
-    example: 'user'
+    example: 'user',
   })
   role: string;
+  @ApiProperty({
+    description: 'Cep',
+    example: '0000000',
+  })
+  cep: string;
 
-  @ApiProperty({ type: CreateAddressDto })
-  address: CreateAddressDto;
+  @ApiProperty({
+    description: 'Bairro',
+    example: 'Jardim exemplo',
+  })
+  district: string;
+
+  @ApiProperty({
+    description: 'Rua',
+    example: 'Rua exemplo',
+  })
+  road: string;
+
+  @ApiProperty({
+    description: 'número da residência',
+    example: 300,
+  })
+  number: number;
 }
--- src/signup/dto/update-user.dto.ts
+++ src/signup/dto/update-user.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto, CreateAddressDto } from './create-user.dto';
+import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
-export class UpdateAddressDto extends PartialType(CreateAddressDto) {}
