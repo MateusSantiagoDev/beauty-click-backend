38058183d3d16e1cde45bb3b348563067a78442e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-28 16:03:01 -0300

    feat: add SignupRepository

--- src/signup/signup.module.ts
+++ src/signup/signup.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { SignupRepository } from './repository/signup-repository';
 import { SignupController } from './signup.controller';
 import { SignupService } from './signup.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [SignupController],
-  providers: [SignupService],
+  providers: [SignupService, SignupRepository],
   exports: [SignupService],
 })
 export class SignupModule {}
