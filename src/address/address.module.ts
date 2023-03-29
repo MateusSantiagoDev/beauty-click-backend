56faeda86b1a093806e763fe601a9dac52a6a444
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-28 23:06:31 -0300

    refactor: add AddressController in addressModule method

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { AddressController } from "./address.controller";
 import { AddressService } from "./address.service";
 import { AddressRepository } from "./repository/address-repository";
 
 @Module({
     imports: [PrismaModule],
+    controllers: [AddressController],
     providers: [AddressService, AddressRepository],
 })
 export class AddressModule {}
