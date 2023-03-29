26691c5dc64d197b2203f9996be439de44df2554
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-28 23:09:37 -0300

    feat: update in UpdateAddresDto method

--- src/address/dto/update-address.dto.ts
+++ src/address/dto/update-address.dto.ts
@@ -1,4 +1,9 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, ApiProperty, ApiHideProperty } from '@nestjs/swagger';
 import { CreateAddressDto } from './create-address.dto';
 
-export class UpdateAddressDto extends PartialType(CreateAddressDto) {}
+export class UpdateAddressDto extends PartialType(CreateAddressDto) {
+    // omitindo a chave userId no metodo de update do swagger
+    @ApiHideProperty()
+    @ApiProperty({ readOnly: true })
+    userId: string;
+}
