aead163e508c663385e996c34b74c6a315e1923c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-30 15:17:46 -0300

    feat: add Prisma to Location methods

--- src/location/repository/location-repository.ts
+++ src/location/repository/location-repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../../prisma/prisma.service";
 
 @Injectable()
 export class LocationRepository {
-    constructor (private readonly prisma) {}
+    constructor (private readonly prisma: PrismaService) {}
 }
