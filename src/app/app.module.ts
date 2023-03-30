185976ff93538b80441ff5716f77b63a823e0940
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-30 15:08:55 -0300

    feat: create LocationModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { LocationModule } from '../location/location.module';
 import { AddressModule } from '../address/address.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SignupModule } from '../signup/signup.module';
@@ -6,7 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule, SignupModule, AddressModule],
+  imports: [PrismaModule, SignupModule, AddressModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
