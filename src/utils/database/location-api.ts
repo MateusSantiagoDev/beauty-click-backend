caf0c4d8c6a0ebaf5a438bc4750fb23401d5daae
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-04 19:18:34 -0300

    refactor: method name change

--- src/utils/database/location-api.ts
+++ src/utils/database/location-api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const LocationMethod = async (address: string): Promise<any> => {
+export const LocationApi = async (address: string): Promise<any> => {
   const url = process.env.BASE_URL;
   const key = process.env.API_KEY;
 
