aa4048827077228b07a7d19ce4128ac6eb0e573d
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-04 19:20:31 -0300

    feat: create LocationModel method

---
+++ src/utils/helpers/location-model.ts
@@ -0,0 +1,18 @@
+import { randomUUID } from 'crypto';
+import { AddressEntity } from '../../location/entities/location-entity';
+
+export const LocationModel = (result: any, address: string): AddressEntity => ({
+  id: randomUUID(),
+  address: address,
+  latitude: result.geometry.location.lat.toString(),
+  longitude: result.geometry.location.lng.toString(),
+  formattedAddress: result.formatted_address,
+  street: null,
+  number: null,
+  neighborhood: null,
+  postalCode: null,
+  city: null,
+  state: null,
+  country: null,
+  createdAt: new Date(),
+});
