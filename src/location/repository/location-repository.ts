8cd013cfea9836262313b501a4d3459b2603689f
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-04 13:43:35 -0300

    feat: create location repository

--- src/location/repository/location-repository.ts
+++ src/location/repository/location-repository.ts
@@ -1,7 +1,12 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../../prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AddressEntity } from '../entities/location-entity';
 
 @Injectable()
 export class LocationRepository {
-    constructor (private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAddressInfo(address: AddressEntity): Promise<AddressEntity> {
+    return await this.prisma.location.create({ data: address });
+  }
 }
