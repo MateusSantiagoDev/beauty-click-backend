e7ba17492094ad5d6e35f0f48d372ce1cd45eed2
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-04 22:58:12 -0300

    refactor: update LocationApi method

--- src/utils/database/location-api.ts
+++ src/utils/database/location-api.ts
@@ -4,10 +4,15 @@ export const LocationApi = async (address: string): Promise<any> => {
   const url = process.env.BASE_URL;
   const key = process.env.API_KEY;
 
-  return await axios.get(url, {
+  const response = await axios.get(url, {
     params: {
       address,
       key,
     },
   });
-}
+  if (response.data.results && response.data.results.length > 0) {
+    return response.data.results[0];
+  } else {
+    return undefined;
+  }
+};
