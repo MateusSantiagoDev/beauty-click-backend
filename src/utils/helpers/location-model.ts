8c277968ca857c62f67d1d4178ef3817456afbd6
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-05 23:17:04 -0300

    refactor: update in LocatiomModel method

--- src/utils/helpers/location-model.ts
+++ src/utils/helpers/location-model.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'crypto';
 import { AddressEntity } from '../../location/entities/location-entity';
 
-export const LocationModel = (result: any, address: string): AddressEntity => ({
+export const LocationModel = (result: any, address: string, id: string): AddressEntity => ({
   id: randomUUID(),
   address: address,
   latitude: result.geometry.location.lat.toString(),
@@ -15,4 +15,5 @@ export const LocationModel = (result: any, address: string): AddressEntity => ({
   state: null,
   country: null,
   createdAt: new Date(),
+  addressDataId: id,
 });
