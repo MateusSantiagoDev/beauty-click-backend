701b45b2ebafa65eecc0be1348e7a22c1647d1d2
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-06 10:45:21 -0300

    feat: create ServicesRepository

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
+import { ServicesRepository } from './repository/services-repository';
 
 @Module({
     imports: [],
     controllers: [ServicesController],
-    providers: [ServicesService],
+    providers: [ServicesService, ServicesRepository],
 })
 export class ServicesModule {};
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { ServicesRepository } from './repository/services-repository';
 
 @Injectable()
 export class ServicesService {
-    constructor(private readonly repository) {}
+    constructor(private readonly repository: ServicesRepository) {}
 }
