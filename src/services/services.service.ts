86150b880d7ed40dfb400d7126b9b75ae46e91f0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-06 12:07:46 -0300

    feat: add validation to create method in ServicesService

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -4,16 +4,36 @@ import { UpdateServicesDto } from './dto/update-services.dto';
 import { CreateServicesDto } from './dto/create-services.dto';
 import { ServicesEntity } from './entities/services-entity';
 import { randomUUID } from 'crypto';
+import { ValidationRequiredFields } from '../utils/helpers/required-fields';
+import { ValidationMethods } from '../utils/helpers/validation-methods';
+import { Exceptions } from '../utils/exceptions/exception';
+import { ExceptionType } from '../utils/exceptions/exceptions-protocols';
 
 @Injectable()
 export class ServicesService {
   constructor(private readonly repository: ServicesRepository) {}
 
   async create(dto: CreateServicesDto): Promise<ServicesEntity> {
+    const requiredFields = ['name', 'image'];
+
+    ValidationRequiredFields(dto, requiredFields);
+
+    await this.repository.getUserById(dto.userId);
+
     const servicesModel: ServicesEntity = {
       id: randomUUID(),
       ...dto,
     };
+
+    const services = new ValidationMethods(this.repository, dto);
+    const user = services.checkAuthorization(servicesModel.userId);
+    if (!(await user)) {
+      throw new Exceptions(
+        ExceptionType.UnauthorizedException,
+        'O cadastro de endereço é somente para empresas ou prestadores de serviço',
+      );
+    }
+
     return await this.repository.create(servicesModel);
   }
 
@@ -35,5 +55,4 @@ export class ServicesService {
     await this.findOne(id);
     await this.repository.delete(id);
   }
-  
 }
