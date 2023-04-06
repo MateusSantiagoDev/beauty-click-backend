a7828c775a7193d17074a55c69cf69234a11fc86
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-06 10:48:08 -0300

    feat: add prisma in services route

--- src/services/repository/services-repository.ts
+++ src/services/repository/services-repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class ServicesRepository {
-    constructor(private readonly prisma) {}
+    constructor(private readonly prisma: PrismaService) {}
 }
--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
 import { ServicesRepository } from './repository/services-repository';
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
-    imports: [],
+    imports: [PrismaModule],
     controllers: [ServicesController],
     providers: [ServicesService, ServicesRepository],
 })
