a141f099db9266fa41475e7db340f54dca469108
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-06 10:34:35 -0300

    feat: create CreateServiceDto method

---
+++ src/services/dto/create-services.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateServicesDto {
+  @ApiProperty({
+    description: 'Tipo de serviço',
+    example: 'Limpeza de pele',
+  })
+  name: string;
+
+  @ApiProperty({
+    description: 'imagem do serviço',
+    example: 'http://foto.com',
+  })
+  image: string;
+
+  @ApiProperty({
+    description: 'ID do usuário com permissão de Perstador de serviço'
+  })
+  userId: string;
+}
