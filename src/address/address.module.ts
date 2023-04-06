57676ba15d5cc55f303d30c2df7c420acf4e7710
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-05 23:07:59 -0300

    feat: add LocationModule in AdddressModule

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
-import { PrismaModule } from "src/prisma/prisma.module";
+import { PrismaModule } from "../prisma/prisma.module";
 import { AddressController } from "./address.controller";
 import { AddressService } from "./address.service";
 import { AddressRepository } from "./repository/address-repository";
+import { LocationModule } from "../location/location.module";
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, LocationModule],
     controllers: [AddressController],
     providers: [AddressService, AddressRepository],
     exports: [AddressRepository],
