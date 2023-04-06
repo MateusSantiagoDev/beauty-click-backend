9c4a89210151d5d3eae60a010449e8e589af3656
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-06 10:38:40 -0300

    feat: create ServicesModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AddressModule } from '../address/address.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SignupModule } from '../signup/signup.module';
 import { AppController } from './app.controller';
+import { ServicesModule } from '../services/services.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule, SignupModule, AddressModule, LocationModule],
+  imports: [PrismaModule, SignupModule, AddressModule, LocationModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
