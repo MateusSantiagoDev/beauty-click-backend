41466c441ce5d78b8b35bc13c7a05ce24b42cfd5
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-06 11:15:23 -0300

    feat: create services methods in ServicesService

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,7 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { ServicesRepository } from './repository/services-repository';
+import { UpdateServicesDto } from './dto/update-services.dto';
+import { CreateServicesDto } from './dto/create-services.dto';
+import { ServicesEntity } from './entities/services-entity';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class ServicesService {
-    constructor(private readonly repository: ServicesRepository) {}
+  constructor(private readonly repository: ServicesRepository) {}
+
+  async create(dto: CreateServicesDto): Promise<ServicesEntity> {
+    const servicesModel: ServicesEntity = {
+      id: randomUUID(),
+      ...dto,
+    };
+    return await this.repository.create(servicesModel);
+  }
+
+  async findAll(): Promise<ServicesEntity[]> {
+    return await this.repository.findAll();
+  }
+
+  async findOne(id: string): Promise<ServicesEntity> {
+    return await this.repository.findOne(id);
+  }
+
+  async update(id: string, dto: UpdateServicesDto): Promise<ServicesEntity> {
+    await this.findOne(id);
+    const servicesModel: Partial<ServicesEntity> = { ...dto };
+    return await this.repository.update(id, servicesModel);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.findOne(id);
+    await this.repository.delete(id);
+  }
+  
 }
