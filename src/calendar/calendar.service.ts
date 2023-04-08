99531f61f1c6cbd28873ba5fe8e88ce9a219cb89
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-07 23:17:08 -0300

    feat: add methods in CalendarService

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -1,7 +1,41 @@
-import { Injectable } from "@nestjs/common";
-import { CalendarRepository } from "./repository/calendar-repository";
+import { Injectable } from '@nestjs/common';
+import { CalendarRepository } from './repository/calendar-repository';
+import { UpdateCalendarDto } from './dto/update-calendar.dto';
+import { CreateCalendarDto } from './dto/create-calendar.dto';
+import { CalendarEntity } from './entities/calendar-entity';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class CalendarService {
-    constructor(private readonly repository: CalendarRepository) {}
+  constructor(private readonly repository: CalendarRepository) {}
+
+  async create(dto: CreateCalendarDto): Promise<CalendarEntity> {
+    const calendar: CalendarEntity = {
+      id: randomUUID(),
+      ...dto,
+    };
+    return await this.repository.create(calendar);
+  }
+
+  async findAll(): Promise<CalendarEntity[]> {
+    return await this.repository.findAll()
+  }
+
+  async findOne(id: string): Promise<CalendarEntity> {
+    return await this.repository.findOne(id)
+  }
+
+  async update(id: string, dto: UpdateCalendarDto): Promise<CalendarEntity> {
+    await this.findOne(id)
+
+    const calendar: Partial<CalendarEntity> = {
+      ...dto,
+    }
+    return await this.repository.update(id, calendar)
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.findOne(id)
+    await this.repository.delete(id)
+  }
 }
