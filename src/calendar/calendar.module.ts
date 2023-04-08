72e3582be6c12220db920e95c2e16a7e39dd9243
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-07 21:37:17 -0300

    feat: create CalendarController

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CalendarController } from './calendar.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [CalendarController],
   providers: [],
 })
 export class CalendarModule {}
