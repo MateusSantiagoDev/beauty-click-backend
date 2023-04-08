b4665031b8fb80e24fd80744afd31e1cac45fc9a
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-07 21:46:52 -0300

    feat: create CalendarService

--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from "@nestjs/common";
+import { CalendarService } from "./calendar.service";
 
 @Controller()
 export class CalendarController {
-    constructor(private readonly service) {}
+    constructor(private readonly service: CalendarService) {}
 }
--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CalendarController } from './calendar.controller';
+import { CalendarService } from './calendar.service';
 
 @Module({
   imports: [],
   controllers: [CalendarController],
-  providers: [],
+  providers: [CalendarService],
 })
 export class CalendarModule {}
