1986df33750e53a1b185b0e76f41ead8976dd6cf
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-07 21:50:10 -0300

    add prisma in calendar route

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CalendarController } from './calendar.controller';
 import { CalendarService } from './calendar.service';
 import { CalendarRepository } from './repository/calendar-repository';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [CalendarController],
   providers: [CalendarService, CalendarRepository],
 })
