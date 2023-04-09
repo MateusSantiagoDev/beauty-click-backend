cb21f29543645ce89d6f56162a0f88d8f7926cae
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-09 17:23:07 -0300

    refactor: update in CreateCalendarDto

--- src/calendar/dto/create-calendar.dto.ts
+++ src/calendar/dto/create-calendar.dto.ts
@@ -3,13 +3,13 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateCalendarDto {
   @ApiProperty({
     description: 'Dia disponível para agendamento',
-    example: '[segunda-feira, terça-feira, quarta-feira]',
+    example: '["segunda-feira", "terça-feira", "quarta-feira"]',
   })
   day: string[];
 
   @ApiProperty({
     description: 'Horário de início do atendimento',
-    example: '[08:00hrs, 09:00hrs, 10:00hrs]',
+    example: '["hh:mm:ss"]',
   })
   startTime: string[];
 
