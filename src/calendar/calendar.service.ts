b91c151b520c9a20135e5cea8869e3f58b644a7e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-09 17:22:11 -0300

    feat: add error handling in CalandarService

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -4,12 +4,43 @@ import { UpdateCalendarDto } from './dto/update-calendar.dto';
 import { CreateCalendarDto } from './dto/create-calendar.dto';
 import { CalendarEntity } from './entities/calendar-entity';
 import { randomUUID } from 'crypto';
+import { ValidationRequiredFields } from '../utils/helpers/required-fields';
+import { Exceptions } from 'src/utils/exceptions/exception';
+import { ExceptionType } from 'src/utils/exceptions/exceptions-protocols';
 
 @Injectable()
 export class CalendarService {
   constructor(private readonly repository: CalendarRepository) {}
 
   async create(dto: CreateCalendarDto): Promise<CalendarEntity> {
+    const requiredFields = ['day', 'startTime'];
+    ValidationRequiredFields(dto, requiredFields);
+
+    await this.repository.getAddressById(dto.addressId);
+
+    const fields = [
+      'segunda-feira',
+      'terça-feira',
+      'quarta-feira',
+      'quinta-feira',
+      'sexta-feira',
+      'sabado',
+      'domingo',
+    ];
+
+    // verificando se existe day, se ele faz parte do objeto fields e se ele é único
+    if (dto.day.length === 0 ||
+      !dto.day.every((day) => fields.includes(day)) ||
+      dto.day.length !== new Set(dto.day).size) {
+    throw new Exceptions(ExceptionType.InvalidData, 'dia inválido!');
+  } 
+
+    // verifica existe hora, se a hora tem o formato correto e se é única
+     const timeRegex = /^(0\d|1\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
+    if(dto.startTime.length === 0 || !dto.startTime.every((time) => timeRegex.test(time)) || dto.startTime.length !== new Set(dto.startTime).size) {
+      throw new Exceptions(ExceptionType.InvalidData, 'hora invalida!');
+    }
+
     const calendar: CalendarEntity = {
       id: randomUUID(),
       day: Array.isArray(dto.day) ? dto.day : [dto.day],
