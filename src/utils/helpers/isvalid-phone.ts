7f8a60c83b5e4efc07cff6b7bda8b0cc818bd995
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-10 13:36:36 -0300

    feat: update in IsValidPhone method

--- src/utils/helpers/isvalid-phone.ts
+++ src/utils/helpers/isvalid-phone.ts
@@ -1,7 +1,8 @@
-export const IsValidPhone = (phone: string): boolean => {
+export const IsValidPhone = (phone: string | string[]): boolean => {
   const pattern = /^\(\d{2}\) \d{5}-\d{4}$/;
-  if (!pattern.test(phone)) {
-    return false;
+  if (Array.isArray(phone)) {
+    return phone.every((value) => pattern.test(value));
+  } else {
+    return pattern.test(phone);
   }
-  return true;
 };
