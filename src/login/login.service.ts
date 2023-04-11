816e538c48514437dc913796dfea864a2a349595
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-10 23:27:22 -0300

    feat: update in loginService

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,7 +1,48 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { LoginRepository } from './repository/login-repository';
+import { LoginDto } from './dto/login.dto';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { ValidationRequiredFields } from '../utils/helpers/required-fields';
+import { Exceptions } from '../utils/exceptions/exception';
+import { ExceptionType } from '../utils/exceptions/exceptions-protocols';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class LoginService {
-    constructor(private readonly repository: LoginRepository) {}
+  constructor(
+    private readonly repository: LoginRepository,
+    private readonly jwt: JwtService,
+  ) {}
+
+  async login(dto: LoginDto): Promise<LoginResponseDto> {
+    const requiredFields = ['email', 'password'];
+
+    ValidationRequiredFields(dto, requiredFields);
+    
+    const { email, password } = dto;
+    const user = await this.repository.findOne(email);
+    if (!user) {
+      throw new Exceptions(
+        ExceptionType.InternalServerErrorException,
+        'Email e/ou senha Inválidos!',
+      );
+    }
+
+    const isValidHash = await compare(password, user.password);
+    if (!isValidHash) {
+      throw new Exceptions(
+        ExceptionType.UnauthorizedException,
+        'Email e/ou senha Inválidos!',
+      );
+    }
+
+    delete user.password;
+    delete user.role;
+
+    return {
+      token: this.jwt.sign({ email }),
+      user,
+    };
+  }
 }
