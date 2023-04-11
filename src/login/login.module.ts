83d9845642e0bf49c1807a22a375f08773670dca
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-11 00:15:50 -0300

    feat: add passaportModule and jwtStrategy

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -4,14 +4,17 @@ import { LoginService } from './login.service';
 import { LoginRepository } from './repository/login-repository';
 import { PrismaModule } from '../prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport'
+import { JwtStrategy } from './jwt/jwt-strategy';
 
 @Module({
-  // secrete e tempo q o token será valido
-  imports: [PrismaModule, JwtModule.register({
+  imports: [PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
     secret: process.env.JWT_SECRET,
     signOptions: { expiresIn: '24h' },
   })],
   controllers: [LoginController],
-  providers: [LoginService, LoginRepository],
+  providers: [LoginService, LoginRepository, JwtStrategy],
 })
 export class LoginModule {}
