09c975da7dda0109af5f1b8622d3a6cfef2b878a
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-10 23:27:00 -0300

    feat: add jwtModule

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -3,9 +3,14 @@ import { LoginController } from './login.controller';
 import { LoginService } from './login.service';
 import { LoginRepository } from './repository/login-repository';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  // secrete e tempo q o token será valido
+  imports: [PrismaModule, JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '24h' },
+  })],
   controllers: [LoginController],
   providers: [LoginService, LoginRepository],
 })
