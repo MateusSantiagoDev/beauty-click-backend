d0ce371e3cb75fd55456a68e0800d4fa7177fea7
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-10 21:16:19 -0300

    feat: create LoginModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { SignupModule } from '../signup/signup.module';
 import { AppController } from './app.controller';
 import { ServicesModule } from '../services/services.module';
 import { CalendarModule } from '../calendar/calendar.module';
+import { LoginModule } from '../login/login.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule, SignupModule, AddressModule, LocationModule, ServicesModule, CalendarModule],
+  imports: [PrismaModule, SignupModule, AddressModule, LocationModule, ServicesModule, CalendarModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
