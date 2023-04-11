02dd3f185459479546096dc208d1038af5c7b3c6
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-10 23:27:51 -0300

    feat: create findOne method

--- src/login/repository/login-repository.ts
+++ src/login/repository/login-repository.ts
@@ -1,7 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { UserEntity } from '../../signup/entities/user-entity';
 
 @Injectable()
 export class LoginRepository {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findOne(email: string): Promise<UserEntity> {
+    try {
+      return await this.prisma.user.findFirstOrThrow({
+        where: { email },
+      });
+    } catch (err) {
+      null;
+    }
+  }
 }
