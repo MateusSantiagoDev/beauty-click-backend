01cd1b07654c43c7558bcf3ff05259b398802ddf
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-14 22:50:15 -0300

    feat: create scheduleDto

---
+++ src/schedule/dto/create-schedule.dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateScheduleDto {
+  @ApiProperty({
+    description: 'ID do usuário que irá agendar um atendimento',
+    example: 'string',
+  })
+  userId: string;
+
+  @ApiProperty({
+    description: 'ID do Estabelecimento',
+    example: 'string',
+  })
+  addressId: string;
+
+  @ApiProperty({
+    description: 'nomes dos Serviços que seram agendados',
+    example: '["limpeza de pele", "corte degrad"]',
+  })
+  serviceName: string[];
+
+  @ApiProperty({
+    description: 'ID do calendario de agendamento',
+    example: 'string',
+  })
+  calendarId: string;
+
+  @ApiProperty({
+    description: 'Dia do agendamento',
+    example: '["sexta-feira", "sabado"]',
+  })
+  day: string[];
+
+  @ApiProperty({
+    description: 'Horário do agendamento',
+    example: '["08:00:00", "09:00:00", "10:00:00"]',
+  })
+  startTime: string[];
+}
---
+++ src/schedule/dto/update-schedule.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty, PartialType, ApiHideProperty } from '@nestjs/swagger';
+import { CreateScheduleDto } from './create-schedule.dto';
+
+export class UpdateScheduleDto extends PartialType(CreateScheduleDto) {
+  @ApiHideProperty()
+  @ApiProperty({ readOnly: true })
+  userId: string;
+}
