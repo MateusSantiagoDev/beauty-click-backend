c07f51da3fb3959fcc7380fc31a1c4a4d62a5049
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-14 22:48:34 -0300

    feat: create schedule  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppController } from './app.controller';
 import { ServicesModule } from '../services/services.module';
 import { CalendarModule } from '../calendar/calendar.module';
 import { LoginModule } from '../login/login.module';
+import { ScheduleModule } from '../schedule/schedule.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule, SignupModule, AddressModule, LocationModule, ServicesModule, CalendarModule, LoginModule],
+  imports: [PrismaModule, SignupModule, AddressModule, LocationModule, ServicesModule, CalendarModule, LoginModule, ScheduleModule],
   controllers: [AppController],
   providers: [AppService],
 })
