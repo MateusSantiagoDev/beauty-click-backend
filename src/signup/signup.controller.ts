896015593cdada129cced9c4aa5cd9e233f03d95
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-14 22:51:46 -0300

    feat: update Signupcontroller

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -19,8 +19,6 @@ import { HandleExceptions } from '../utils/exceptions/handle-exceptions';
 import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Signup')
-@UseGuards(AuthGuard())
-@ApiBearerAuth()
 @Controller('signup')
 export class SignupController {
   constructor(private readonly service: SignupService) {}
@@ -33,37 +31,43 @@ export class SignupController {
     try {
       return await this.service.create(dto);
     } catch (err) {
-      HandleExceptions(err)
+      HandleExceptions(err);
     }
   }
 
   @ApiOperation({
     summary: 'Buscar todos os usuário',
   })
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
   @Get()
   async findAll(): Promise<UserEntity[]> {
     try {
       return await this.service.findAll();
     } catch (err) {
-      HandleExceptions(err)
+      HandleExceptions(err);
     }
   }
 
   @ApiOperation({
     summary: 'Buscar um usuário pelo ID',
   })
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<UserEntity> {
     try {
       return await this.service.findOne(id);
     } catch (err) {
-      HandleExceptions(err)
+      HandleExceptions(err);
     }
   }
 
   @ApiOperation({
     summary: 'Atualizar um usuário pelo ID',
   })
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -72,20 +76,22 @@ export class SignupController {
     try {
       return await this.service.update(id, dto);
     } catch (err) {
-      HandleExceptions(err)
+      HandleExceptions(err);
     }
   }
 
   @ApiOperation({
     summary: 'Remover um usuário pelo ID',
   })
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<void> {
     try {
       await this.service.delete(id);
     } catch (err) {
-      HandleExceptions(err)
+      HandleExceptions(err);
     }
   }
 }
