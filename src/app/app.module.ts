4d959725e91af8ba9b6562df684e5a152cd04ae3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-17 11:02:23 -0300

    feat: create addresslocation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { LocationModule } from '../location/location.module'; 
+import { LocationModule } from '../location/location.module';
 import { AddressModule } from '../address/address.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SignupModule } from '../signup/signup.module';
@@ -8,10 +8,21 @@ import { ServicesModule } from '../services/services.module';
 import { CalendarModule } from '../calendar/calendar.module';
 import { LoginModule } from '../login/login.module';
 import { ScheduleModule } from '../schedule/schedule.module';
+import { AddressLocationModule } from '../address-location/address-location.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule, SignupModule, AddressModule, LocationModule, ServicesModule, CalendarModule, LoginModule, ScheduleModule],
+  imports: [
+    PrismaModule,
+    SignupModule,
+    AddressModule,
+    LocationModule,
+    ServicesModule,
+    CalendarModule,
+    LoginModule,
+    ScheduleModule,
+    AddressLocationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
