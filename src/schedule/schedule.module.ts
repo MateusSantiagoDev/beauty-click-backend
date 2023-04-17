81107b3ab125846e35023c52af440323d749b467
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-17 00:40:49 -0300

    fix: update in scheduleModule

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
 import { ScheduleController } from "./schedule.controller";
 import { ScheduleService } from "./schedule.service";
 import { ScheduleRepository } from "./repository/schedule-repository";
 import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
- imports: [PrismaModule],
+ imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
  controllers: [ScheduleController],
  providers: [ScheduleService, ScheduleRepository],
 })
