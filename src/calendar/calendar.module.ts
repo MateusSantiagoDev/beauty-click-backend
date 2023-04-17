f02f8f60e7e677e1b81912a85ce5ca9a527f582c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-17 00:40:23 -0300

    fix: update in calendarModule

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { CalendarController } from './calendar.controller';
 import { CalendarService } from './calendar.service';
 import { CalendarRepository } from './repository/calendar-repository';
 import { PrismaModule } from '../prisma/prisma.module';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' }),],
+  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [CalendarController],
   providers: [CalendarService, CalendarRepository],
 })
