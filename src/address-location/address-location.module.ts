7036ebaf83148d6c38d4edb2e3d974f53e618f1e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-04-17 11:04:05 -0300

    feat: create addresslocationController

--- src/address-location/address-location.module.ts
+++ src/address-location/address-location.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
+import { AddressLocationController } from "./address-location.controller";
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [AddressLocationController],
   providers: [],
 })
 export class AddressLocationModule {}
