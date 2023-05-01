8f05eb42b70e164506adad2795923eb1abc574c3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-05-01 16:52:13 -0300

    feat: update in address-location route

--- src/address-location/address-location.controller.ts
+++ src/address-location/address-location.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from "@nestjs/common";
-import { AddressLocartionService } from "./address-location.service";
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { AddressLocartionService } from './address-location.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { CreateAddressLocationDto } from './dto/create-address-location.dto';
+import { AddressLocationEntity } from './entities/address-location-entity';
 
-@Controller()
+@ApiTags('Places')
+@Controller('places')
 export class AddressLocationController {
-    constructor(private readonly service: AddressLocartionService) {}
+  constructor(private readonly service: AddressLocartionService) {}
+
+  @ApiOperation({
+    summary: 'Buscar endereços próximos',
+  })
+  @Get(':neighborhood')
+  async create(
+    @Query('neighborhood') neighborhood: CreateAddressLocationDto,
+  ): Promise<AddressLocationEntity> {
+    try {
+      return await this.service.create(neighborhood);
+    } catch (err) {}
+  } 
 }
