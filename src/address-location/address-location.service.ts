ad533aec5e7ca17bd2a102b69c9333f877abcfec
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-05-01 16:53:13 -0300

    feat: update in address-location service

--- src/address-location/address-location.service.ts
+++ src/address-location/address-location.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { AddressLocationRepository } from './repository/address-location-repository';
+import { CreateAddressLocationDto } from './dto/create-address-location.dto';
+import { AddressLocationEntity } from './entities/address-location-entity';
+import { randomUUID } from 'crypto';
+import { PlacesLocationApi } from '../utils/database/places-api'; 
 
 @Injectable()
 export class AddressLocartionService {
   constructor(private readonly repository: AddressLocationRepository) {}
+
+  async create(
+    neighborhood: CreateAddressLocationDto,
+  ): Promise<AddressLocationEntity> {
+    const result = await PlacesLocationApi(neighborhood, this.repository)
+
+    const places: any = {
+      id: randomUUID(),
+      ...result,
+    };
+    return await this.repository.findAll(places);
+  } 
 }
