1052e767c31ed534dae55a1be6066e32ee62d27c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-05-01 16:54:38 -0300

    feat: update in address-location repository

--- src/address-location/repository/address-location-repository.ts
+++ src/address-location/repository/address-location-repository.ts
@@ -1,7 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AddressLocationEntity } from '../entities/address-location-entity';
+import { AddressEntity } from '../../address/entities/address-entity';
 
 @Injectable()
 export class AddressLocationRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(data: AddressLocationEntity): Promise<AddressLocationEntity> {
+    try {
+      return await this.prisma.addressLocation.create({ data });
+    } catch (err) {}
+  }
+
+  async findByNeighborhood(neighborhood: string): Promise<AddressEntity[]> {
+    try {
+      return await this.prisma.address.findMany({
+        where: {
+          neighborhood: {
+            contains: neighborhood,
+          },
+        },
+      });
+    } catch (err) {
+      null;
+    }
+  }
+
+  async getSavedAddresses(): Promise<any> {
+    try {
+      return await this.prisma.address.findMany();
+    } catch (err) {}
+  }
+
+  async getLocationByAddress(neighborhood): Promise<any> {
+    try {
+      return await this.prisma.location.findMany({
+        where: { neighborhood: { equals:neighborhood }}
+      });
+    } catch (err) {}
+  }
 }
